97334a90c1b76aaa603607dbf4fe01ed7e7e9625
Juan Luis Chaurant <juanluischaurant@gmail.com>
2019-11-14 17:41:16 -0400

    Creado controlador de usuarios

---
+++ application/controllers/gestion/Usuarios.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Gestiona lo referente a Usuarios que pueden utilizar
+ * CE Gestión, cada uno con nivel de accesibilidad asignado
+ * 
+ * @author Juan Luis Chaurant <juanluischaurant@gmail.com>
+ */
+class Usuarios extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        
+        // Si el usuario no está logeado
+		if(!$this->session->userdata('login'))
+		{
+			// redirigelo al inicio de la aplicación
+            redirect(base_url());
+        }
+        else
+        {
+            // Carga el controlador
+            $this->load->model("Pagos_model");
+        }
+    }
