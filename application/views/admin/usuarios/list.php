e871564b8e2524710ae55c94d6c12d6a3a0f969c
Juan Luis Chaurant <juanluischaurant@gmail.com>
2019-11-20 15:54:55 -0400

    Añadida vista de detalles de Usuario

--- application/views/admin/usuarios/list.php
+++ application/views/admin/usuarios/list.php
@@ -86,13 +86,13 @@
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title">Informacion del Usuario</h4>
+        <h4 class="modal-title text-center">Informacion del Usuario</h4>
       </div>
       <div class="modal-body">
         
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-danger pull-left" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-danger center-block" data-dismiss="modal">Cerrar</button>
       </div>
     </div>
     <!-- /.modal-content -->
